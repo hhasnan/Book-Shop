36bbd81ca0a17f8209cc393d648cf290059bccde
hhasnan <hhasnan400@gmail.com>
2025-07-23 12:43:17 +0500

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
                 <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
                     {/* <span className="font-semibold text-xl tracking-tight">Book Shop</span> */}
-                    <img src="../../public/icon-black.png" alt="" className='h-[10dvh]' />
+                    <img src="./icon-black.png" alt="" className='h-[10dvh]' />
                 </div>
                 {/* Mobile menu toggle */}
                 <div className="block lg:hidden">
